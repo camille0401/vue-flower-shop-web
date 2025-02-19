1c5cd4a875245b25661202bd2d74bef1dd80b371
wangdery <wangdery819@gmail.com>
2025-02-19 16:36:57 +0800

    variable.scss全局引入

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,9 +1,11 @@
 <template>
-    <div>我是首页</div>
+    <div class="test">我是首页</div>
 </template>
 <script setup name="Home">
 
 </script>
 <style scoped lang="scss">
-    
+    .test {
+        color: $priceColor;
+    }
 </style>
